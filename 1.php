672718a946e1736364dac5fd8c58945acc7e99b9
FranciscoMorales1 <146593362+FranciscoMorales1@users.noreply.github.com>
2023-09-30 20:37:07 -0500

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+ <title>Formulario básico</title>
+</head>
+<body>
+
+<form method = "post" action = "2.php">
+
+Hola... 
+
+<br>
+<br>
+<br>
+
+¿Quieres escucharme?
+<br>
+Sí <input type = "radio" name = "palabras" id = "sí" value = "sí"> no <input type = "radio" name = "palabras" id = "no" value = "no">
+
+<br>
+<br>
+
+<input type = "submit" value = "entregar">
+
+<html>
+
+
+
+
+
